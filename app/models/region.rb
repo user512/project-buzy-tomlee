35cc40c08b62ee12709cc84652ec922d11faa4f0
Tom Lee <tlee418@g.ucla.edu>
2015-08-09 01:00:28 -0700

    use location table only, testing google map api and geolocation, the app can now detect user location and show on google map

--- app/models/region.rb
+++
@@ -1,5 +0,0 @@
-class Region < ActiveRecord::Base
-  # Remember to create a migration!
-  has_many :cities
-  has_many :locations, through: :cities
-end
--- public/js/application.js
+++ public/js/application.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+
 
   // For AJAX
   // $('a').click(function() {
@@ -6,20 +6,43 @@ $(document).ready(function() {
   // });
 var x = document.getElementById("demo");
 
-window.onload = function () {
+var startUp = function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.watchPosition(success, failure);
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";}
     }
 
+window.onload = startUp();
+
 function success(position) {
     x.innerHTML="Latitude: " + position.coords.latitude +
     "<br>Longitude: " + position.coords.longitude;
+    var map;
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+      zoom: 12,
+      center: {lat: position.coords.latitude, lng: position.coords.longitude}
+    });
+    google.maps.event.addDomListener(window, 'load', success);
 }
+
 
 function failure(position) {
-  x.innerHTML = "Geolocation is not supported by this browser.";
+  switch(error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
 }
 
-});
+google.maps.event.addDomListener(window, 'load');
+
